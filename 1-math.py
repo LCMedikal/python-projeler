6b40148df32678acf0c2d43a9180dea6a89ff9cb
LCMedikal <sturker07@gmail.com>
2020-01-10 21:04:50 +0300

    Python ile matematiksel işlemler yapıldı.

---
+++ 1-math.py
@@ -0,0 +1,9 @@
+print(5+6)  # 5 ve 6 ssayýsýný topladýk.
+
+print(5/6) # 5 sayýsýný 6 ya böldük.
+
+print(22//9) # 22 sayýsýnýn 9 sayýsýna bölündükten sonra tam sonucunu bulduk
+
+print((5+3)*4) # Matematiksel öncelik sýrasýný parantez ile saðladýk.
+
+print(5**3)  # Matematiksel olarak bir sayýnýn küpünü aldýk.
