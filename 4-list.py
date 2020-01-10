b0d7a7f9a0d14b201e444ddbead0944401947457
LCMedikal <sturker07@gmail.com>
2020-01-10 21:45:20 +0300

    Python listeler uzerine calisildi

---
+++ 4-list.py
@@ -0,0 +1,18 @@
+
+liste_test = ['a','b']
+
+print(liste_test) # listeyi yazdirdik
+
+print(liste_test[0]) # listenin ilk elemanını yazdirdik.
+
+print(len(liste_test)) # listenin uzunlugunu yazdirdik.
+
+liste_test[0] = 'Deneme' # listenin ilk elemanını degistirdik
+
+print(liste_test) # listenin degisiklik yapilmis halini yazdirdik.
+
+liste_test.append('c') # Listeye eleman ekledik.
+
+print(liste_test) # Eleman eklenmis listeyi yazdirdik.
+
+print(liste_test+ ['test']) # Listeye toplama fonksiyonu ile eleman ekleme
