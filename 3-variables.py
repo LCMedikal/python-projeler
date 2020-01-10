13c0241c2ce9207e5cd887a604d0929695f44414
LCMedikal <sturker07@gmail.com>
2020-01-10 21:19:30 +0300

    Degisken tiplerini ekrana yazdirdik.

---
+++ 3-variables.py
@@ -0,0 +1,11 @@
+print(type(12))  # int degiþken tipini yazdirdik.
+
+print(type(3.12)) #float degisken tipini yazdirdik.
+
+print(type([3,5,7,9])) # list deðisken tipini yazdirdik.
+
+print(type((5,7,9))) #tuple degisken tipini yazdirdik.
+
+print(type({"elma":"armut	"}))  # sozluk degisken tipini yazdirdik.	
+
+print(type('deneme')) # string degisken tipini yazdirdik.
