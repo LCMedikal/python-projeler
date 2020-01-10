a5a55ceb0a9f72ab8a080d1f4b4ff369db146b84
LCMedikal <sturker07@gmail.com>
2020-01-10 21:11:47 +0300

    Python string özellikleri üzerine çalışıldı.

---
+++ 2-string.py
@@ -0,0 +1,11 @@
+
+yazi = 'deneme yazisi' # deðiþkene örnek bir yazi tanimladik.
+
+
+print(yazi)
+
+print(yazi[0]) # deðiþkendeki yazi dizisindeki ilk elemaný yazdirdik.
+
+print(yazi[0:5]) #deðiþkendeki yazi sinin ilk 6 elemanini yazdirdik.
+
+print(len(yazi)) # yazi deðikeninin eleman sayisini yazdirdik.
